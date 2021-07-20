7c2e9f8eb214ee843cfa39ccaa242c9bd803be6c
Ekow Bentsi-Enchill <ekow.bentsi.enchill@gmail.com>
2021-07-20 11:29:17 +0000

    beautiful

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'musa';
+
+function upperCastCustomerName()
+{
+    return customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'bob'
+}
+
+const leastFavoriteCustomer = 'mary';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'dagmar';
+}
